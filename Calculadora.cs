bf8f61140ed2483e87209259201b9a76ed69ec53
007juanca <jgarciam114@miumg.edu.gt>
2023-03-03 20:41:34 -0600

    Agregar archivos de proyecto.

---
+++ Calculadora.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafio2
+{
+    internal class Calculadora
+    {
+        public int Sumar(int n1, int n2)
+        {
+            return n1 + n2;
+
+        }
+        public int Restar(int n1, int n2)
+        {
+            return n1 - n2;
+
+        }
+        public int Multiplicar(int n1, int n2, int n3)
+        {
+            return n1 * n2 * n3;
+
+        }
+        public int Dividir(int n1, int n2)
+        {
+            return n1 / n2;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace desafio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calculadora = new Calculadora();
+            int suma = calculadora.Sumar(23, 34);
+            int resta = calculadora.Restar(23, 34);
+            int multiplicacion = calculadora.Multiplicar(4, 2, 4);
+            int division = calculadora.Dividir(36, 6);
+
+            Console.WriteLine("El resultado de la suma es: " + suma);
+            Console.WriteLine("El resultado de resta es: " + resta);
+            Console.WriteLine("resultado de la multiplicación: " + multiplicacion);
+            Console.WriteLine("EL resultado de división es: " + division);
+            Console.Read();
+        }
+    }
+}
